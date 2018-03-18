671c8dd9f16c084c81d8659dd75f29a0ffb4d38c
Joel Llacer <z0beat@gmail.com>
2018-03-18 15:40:43 +0100

    Add boxstarter script

---
+++ wsl/boxstarter.ps1
@@ -0,0 +1,10 @@
+choco install git
+choco install gitkraken
+choco install visualstudiocode
+choco install authy
+choco install spotify
+choco install googlechrome
+choco install firefox
+choco install docker-for-windows --pre
+choco install 7zip
+choco install caffeine
